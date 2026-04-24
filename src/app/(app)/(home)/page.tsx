93f6a45fc526eaf13bde28b772976b68a39ad495
hiverkiya <hiverkiya@gmail.com>
2026-04-24 22:05:40 +0530

    Fixed minor payload type generation error

--- src/app/(app)/(home)/page.tsx
+++ src/app/(app)/(home)/page.tsx
@@ -1,29 +1,11 @@
-import { Button } from "@/components/ui/button";
-import { Checkbox } from "@/components/ui/checkbox";
-import { Input } from "@/components/ui/input";
-import { Progress } from "@/components/ui/progress";
-import { Textarea } from "@/components/ui/textarea";
-
-export default function Home() {
-  return (
-    <div className="p-4">
-      <div className="flex flex-col gap-y-4">
-        <div>
-          <Button variant="elevated">I am a button</Button>
-        </div>
-        <div>
-          <Input placeholder="I am an input" />
-        </div>
-        <div>
-          <Progress value={50} />
-        </div>
-        <div>
-          <Textarea placeholder="I am a textarea" />
-        </div>
-        <div>
-          <Checkbox />
-        </div>
-      </div>
-    </div>
-  );
+import configPromise from "@payload-config";
+import { getPayload } from "payload";
+export default async function Home() {
+  const payload = await getPayload({
+    config: configPromise,
+  });
+  const data = await payload.find({
+    collection: "categories",
+  });
+  return <div>{JSON.stringify(data, null, 2)}</div>;
 }
--- src/payload.config.ts
+++ src/payload.config.ts
@@ -1,17 +1,17 @@
 // storage-adapter-import-placeholder
-import { mongooseAdapter } from '@payloadcms/db-mongodb'
-import { payloadCloudPlugin } from '@payloadcms/payload-cloud'
-import { lexicalEditor } from '@payloadcms/richtext-lexical'
-import path from 'path'
-import { buildConfig } from 'payload'
-import { fileURLToPath } from 'url'
-import sharp from 'sharp'
+import { mongooseAdapter } from "@payloadcms/db-mongodb";
+import { payloadCloudPlugin } from "@payloadcms/payload-cloud";
+import { lexicalEditor } from "@payloadcms/richtext-lexical";
+import path from "path";
+import { buildConfig } from "payload";
+import { fileURLToPath } from "url";
+import sharp from "sharp";
+import { Users } from "./collections/Users";
+import { Media } from "./collections/Media";
+import { Categories } from "./collections/Categories";
 
-import { Users } from './collections/Users.js'
-import { Media } from './collections/Media.js'
-
-const filename = fileURLToPath(import.meta.url)
-const dirname = path.dirname(filename)
+const filename = fileURLToPath(import.meta.url);
+const dirname = path.dirname(filename);
 
 export default buildConfig({
   admin: {
@@ -20,18 +20,18 @@ export default buildConfig({
       baseDir: path.resolve(dirname),
     },
   },
-  collections: [Users, Media],
+  collections: [Users, Media, Categories],
   editor: lexicalEditor(),
-  secret: process.env.PAYLOAD_SECRET || '',
+  secret: process.env.PAYLOAD_SECRET || "",
   typescript: {
-    outputFile: path.resolve(dirname, 'payload-types.ts'),
+    outputFile: path.resolve(dirname, "payload-types.ts"),
   },
   db: mongooseAdapter({
-    url: process.env.DATABASE_URI || '',
+    url: process.env.DATABASE_URI || "",
   }),
   sharp,
   plugins: [
     payloadCloudPlugin(),
     // storage-adapter-placeholder
   ],
-})
+});
